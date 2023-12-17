abeb62e53e947e84426158c316fca2601ee4565b
giannurwana19 <giannurwana19@gmail.com>
2023-12-17 19:31:09 +0700

    siklus hidup test

--- src/test/java/giandev/test/OrderedTest.java
+++ src/test/java/giandev/test/OrderedTest.java
@@ -1,30 +1,50 @@
 package giandev.test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 /**
  * MethodName: diurutkan berdasarkan nama method
  * Random: diurutkan secara random
  * OrderAnnotation: diurutkan sesuai @Order
+ * secara default lifecycle test adalah PER_METHOD
+ * kita bisa ubah menjadi PER_CLASS
+ * Salah satu keuntungan saat menggunakan Lifecycle.PER_CLASS adalah, kita bisa menggunakan
+ * BeforeAll dan AfterAll di method biasa, tidak harus menggunakan function object / static
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+
+    private int counter = 0;
+
+    @BeforeAll
+    void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    void afterAll() {
+        System.out.println("After All");
+    }
+
     @Test
     @Order(1)
     void test3() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(2)
     void test2() {
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(3)
     void test1() {
+        counter++;
+        System.out.println(counter);
     }
 }
